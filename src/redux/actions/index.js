dbecafda714981184935a1b4b4326825152a5abd
David_Izosimov <David.Izosimov@gmail.com>
2020-02-01 16:32:12 +0100

    Adds redux-thunk and creates store for redux.

---
+++ src/redux/actions/index.js
@@ -0,0 +1,10 @@
+import { createStore, compose } from 'redux'
+import rootReducer from '../reducers/index'
+import thunk from 'redux-thunk'
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  storeEnhancers(thunk)
+)
