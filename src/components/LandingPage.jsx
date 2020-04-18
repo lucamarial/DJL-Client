02239639cbdf7547e105068639c65ebaa501990e
luca <l.lobacher@web.de>
2020-04-18 19:21:35 +0200

    Wraps list item in NavLink to SingleArticle

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,34 +1,37 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getData } from '../redux/actions/index'
+import { NavLink } from 'react-router-dom'
 
-const LandingPage = props => {
-  useEffect(() => {
-    props.getData()
-  }, [])
+const LandingPage = (props) => {
+	useEffect(() => {
+		props.getData()
+	}, [])
 
-  return (
-    <>
-      <ul>
-        {props.articles.map(art => (
-          <li id={`article-${art.id}`} key={art.id}>
-            <div id={`title-${art.id}`}>{art.title}</div>
-            <div id={`content-${art.id}`}>{art.content}</div>
-            <div id={`author-${art.id}`}>{art.author}</div>
-            <div id={`publish_date-${art.id}`}>{art.publish_date}</div>
-          </li>
-        ))}
-      </ul>
-      {props.errorMessage ? <p id='error'>{props.errorMessage}</p> : null}
-    </>
-  )
+	return (
+		<>
+			<ul>
+				{props.articles.map((art) => (
+					<NavLink to={`/article/${art.id}`} id={`article-${art.id}`} key={art.id}>
+						<li id={`article-${art.id}`} key={art.id}>
+							<div id={`title-${art.id}`}>{art.title}</div>
+							<div id={`content-${art.id}`}>{art.content}</div>
+							<div id={`author-${art.id}`}>{art.author}</div>
+							<div id={`publish_date-${art.id}`}>{art.publish_date}</div>
+						</li>
+					</NavLink>
+				))}
+			</ul>
+			{props.errorMessage ? <p id='error'>{props.errorMessage}</p> : null}
+		</>
+	)
 }
 
-const mapStateToProps = state => {
-  return {
-    articles: state.articles,
-    errorMessage: state.errorMessage
-  }
+const mapStateToProps = (state) => {
+	return {
+		articles: state.articles,
+		errorMessage: state.errorMessage,
+	}
 }
 
 export default connect(mapStateToProps, { getData })(LandingPage)
