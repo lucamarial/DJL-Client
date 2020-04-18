5e0d8c5b78ad537c6d4da3e65d142ac45a137afb
luca <l.lobacher@web.de>
2020-04-18 19:14:46 +0200

    Creates SingleArticle component and adds route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import SingleArticle from './components/SingleArticle'
 
 const App = () => {
   return (
     <div className="App">
       <Route exact path='/' component={LandingPage} />
+      <Route exact path='/article/:id' component={SingleArticle} />
     </div>
   )
 }
---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleArticle = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default SingleArticle
