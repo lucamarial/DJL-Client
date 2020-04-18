d13ecfb8888d887ef1529cb3194df6ea57b04710
jonas <jonas.masharqa@gmail.com>
2020-04-18 20:41:40 +0200

    Adds specific article action, fixes getData invokation.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,37 +1,41 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { getData } from '../redux/actions/index'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getData } from '../redux/actions/index';
+import { NavLink } from 'react-router-dom';
 
 const LandingPage = (props) => {
-	useEffect(() => {
-		props.getData()
-	}, [])
+  useEffect(() => {
+    props.articles.length > 0 || props.getData();
+  }, []);
 
-	return (
-		<>
-			<ul>
-				{props.articles.map((art) => (
-					<NavLink to={`/article/${art.id}`} id={`article-${art.id}`} key={art.id}>
-						<li id={`article-${art.id}`} key={art.id}>
-							<div id={`title-${art.id}`}>{art.title}</div>
-							<div id={`content-${art.id}`}>{art.content}</div>
-							<div id={`author-${art.id}`}>{art.author}</div>
-							<div id={`publish_date-${art.id}`}>{art.publish_date}</div>
-						</li>
-					</NavLink>
-				))}
-			</ul>
-			{props.errorMessage ? <p id='error'>{props.errorMessage}</p> : null}
-		</>
-	)
-}
+  return (
+    <>
+      <ul>
+        {props.articles.map((art) => (
+          <NavLink
+            to={`/article/${art.id}`}
+            id={`article-${art.id}`}
+            key={art.id}
+          >
+            <li id={`article-${art.id}`} key={art.id}>
+              <div id={`title-${art.id}`}>{art.title}</div>
+              <div id={`content-${art.id}`}>{art.content}</div>
+              <div id={`author-${art.id}`}>{art.author}</div>
+              <div id={`publish_date-${art.id}`}>{art.publish_date}</div>
+            </li>
+          </NavLink>
+        ))}
+      </ul>
+      {props.errorMessage ? <p id='error'>{props.errorMessage}</p> : null}
+    </>
+  );
+};
 
 const mapStateToProps = (state) => {
-	return {
-		articles: state.articles,
-		errorMessage: state.errorMessage,
-	}
-}
+  return {
+    articles: state.articles,
+    errorMessage: state.errorMessage,
+  };
+};
 
-export default connect(mapStateToProps, { getData })(LandingPage)
+export default connect(mapStateToProps, { getData })(LandingPage);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios'
-import { ARTICLES_LOADED, DATA_ERROR } from '../constants'
+import { ARTICLES_LOADED, DATA_ERROR, SPECIFIC_ARTICLE } from '../constants'
 
 export const getData = () => {
   return async dispatch => {
     try {
       let response = await axios.get('http://localhost:3000/api/v1/articles')
       return dispatch({type: ARTICLES_LOADED, payload: response.data.articles})
+    } catch(error) {
+      return dispatch({ type: DATA_ERROR, payload: error.response.data })
+    }
+  }
+}
+
+export const getSpecificArticle = (articleId) => {
+  return async dispatch => {
+    try {
+      let response = await axios.get(`http://localhost:3000/api/v1/articles/${articleId}`)
+      return dispatch({ type: SPECIFIC_ARTICLE, payload: response.data.article })
     } catch(error) {
       return dispatch({ type: DATA_ERROR, payload: error.response.data })
     }
