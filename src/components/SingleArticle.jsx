89ee021f2ee81cdd8634c403337f6898efef31c9
luca <l.lobacher@web.de>
2020-04-19 16:29:32 +0200

    Gets SingleArticle state from redux store and renders article's values

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router'
-import { connect } from 'react-redux'
-import { getSpecificArticle } from '../redux/actions/index'
+import { useParams } from 'react-router';
+import { connect } from 'react-redux';
+import { getSpecificArticle } from '../redux/actions/index';
 
 const SingleArticle = (props) => {
-  let { id } = useParams()
+	let { id } = useParams();
+	let { specificArticle } = props;
 
-  useEffect(() => {
-    props.getSpecificArticle(id)
-  }, [])
+	useEffect(() => {
+		props.getSpecificArticle(id);
+	}, []);
 
-  return (
-    <>
-      
-    </>
-  )
-}
-
-const mapStateToProps = (state) => {
-  return {
-    article: state.article,
-    errorMessage: state.errorMessage
-  };
+	return (
+		<>
+			<div id={`article-${specificArticle.id}`}>
+				<div id={`title-${specificArticle.id}`}>{specificArticle.title}</div>
+				<div id={`content-${specificArticle.id}`}>
+					{specificArticle.content}
+				</div>
+				<div id={`author-${specificArticle.id}`}>{specificArticle.author}</div>
+				<div id={`publish_date-${specificArticle.id}`}>
+					{specificArticle.publish_date}
+				</div>
+			</div>
+		</>
+	);
 };
 
-export default connect(mapStateToProps, { getSpecificArticle })(SingleArticle)
+const mapStateToProps = (state) => {
+	return {
+		specificArticle: state.specificArticle,
+		errorMessage: state.errorMessage,
+	};
+};
+
+export default connect(mapStateToProps, { getSpecificArticle })(SingleArticle);
